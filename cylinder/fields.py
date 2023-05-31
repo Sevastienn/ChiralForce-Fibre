ad5bbbe91f3754c1cc743973ae4ed8a5f896693e
Sevastienn <sebastian.golat@mensa.cz>
2023-05-31 12:56:47 +0100

    fixed discontinuity

--- cylinder/fields.py
+++ cylinder/fields.py
@@ -9,6 +9,11 @@ def material(n_in=1.45,n_out=1):
     global n1; n1=n_in  # Refractive index or sqrt(ϵ₁) inside
     global n2; n2=n_out # Refractive index or sqrt(ϵ₂) outside
 
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
 # Boundary condition matrix
 def A(neff,r0_per_λ0):
     k0_r0=2*np.pi*r0_per_λ0 #k0·r0
@@ -66,7 +71,7 @@ def logdetA(neff,r0_per_λ0):
         a[0,0]*a[1,2]*a[2,1]*a[3,3] - a[0,1]*a[1,0]*a[2,2]*a[3,3] + a[0,0]*a[1,1]*a[2,2]*a[3,3]))
     return np.where((neff<n1) & (neff>n2),output,0)
 
-def get_neff(rmin=0,rmax=2,N=1000):    
+def get_neff(rmin=0,rmax=2,N=1000,neff_fig=False,r0_over_λ0=None):    
     r0_per_λ0=np.linspace(rmin,rmax,N+1,endpoint=False)[1:]
     neff=np.linspace(n2,n1,N+1,endpoint=False)[1:]
     Ro, Ne = np.meshgrid(r0_per_λ0, neff)
@@ -81,97 +86,88 @@ def get_neff(rmin=0,rmax=2,N=1000):
             n[i]=0
 
     # Calculates amplitudes    
-    # S=np.zeros([len(r0_per_λ0)])
-    U=np.zeros([len(r0_per_λ0)])
-    # V=np.zeros([len(r0_per_λ0),4],dtype=complex)
-    # W=np.zeros([len(r0_per_λ0),4],dtype=complex)
-    # g=np.zeros_like(r0_per_λ0)
-    # for i in range(len(r0_per_λ0)):
-    #     tol=3e-4
-    #     # print(np.shape(A(n[i],r0_per_λ0[i])))
-    #     M=np.matrix(A(n[i],r0_per_λ0[i]),dtype=complex)
-    #     v=null_space(M,rcond=tol)
-    #     if np.shape(v)==(4,1): #print('One')
-    #         V[i,:]=v[0]
-    #         g[i]=1
-    #     elif np.shape(v)==(4,2): #print('Two')
-    #         ix=0+0*int(np.round(np.mean(np.argmin(v,axis=1))))
-    #         V[i,:]=v[:, ix]
-    #         g[i]=1
-    #     else: g[i]=np.nan
-    #     W[i,:]=np.matmul(V[i,:],M)
-    #     U[i]=np.log10(np.abs(np.emath.sqrt(np.inner(W[i,:],W[i,:])/np.inner(V[i,:],V[i,:]))))
-    #     S[i]=logdetA(n[i],r0_per_λ0[i])
-    #     # S[i]=np.log10(np.inner(V[i,:],V[i,:]))
-    #     # U[i]=np.log10((np.emath.sqrt(np.inner(W[i,:],W[i,:]))))-1/2*np.log10(np.inner(V[i,:],V[i,:]))
-    #     # print(np.inner(W[i,:],W[i,:]))
-    #     # print(g)
-    #             # Calculates amplitudes
-    V=np.zeros([len(r0_per_λ0),4],dtype=complex)
-    # P=np.zeros([len(r0_per_λ0),4],dtype=complex)
-    W=np.zeros_like(r0_per_λ0,dtype=complex)
-    # for i in range(len(r0_per_λ0)):
-    #     M=np.matrix(A(n[i],r0_per_λ0[i]),dtype=complex)
-    #     (w,v)=np.linalg.eig(M)
-    #     # print_matrix(w)
-    #     # print(w[np.argmin(np.abs(w))])
-    #     # print(v)
-    #     W[i]=np.min(np.abs(w))
-    #     V[i,:]=v[np.argmin(np.abs(w))]
-    #     U[i]=np.abs(np.linalg.det(M))
-    #     # u=np.matmul(M,V[i,:])
-    #     # U[i]=np.real((np.inner(np.conj(u),u)))
-    #     # U[i]=(np.real(np.inner(np.conj(v[np.argmin(np.abs(w))]),v[np.argmin(np.abs(w))])))
-    #     # print(np.matmul(M,V[i,:])/(w[np.argmin(np.abs(w))]*v[np.argmin(np.abs(w))]))
-    # M=np.transpose(A(n,r0_per_λ0),(2,0,1))
-    # (w,v)=np.linalg.eig(M)
-    # print(np.shape(w))
-    # print(np.shape(v))
-    # print(M[500])
-    # print((w)[500,3])
-    # print((v)[500,3])
-    # print(M[i]@(v)[500,:,3])
     M=np.transpose(A(n,r0_per_λ0),(2,0,1))
     (w,v)=np.linalg.eig(M)
-    for i in range(N):
-        # print(np.argmin(np.abs((w)[i])))
-        argus=np.argmin(np.abs((w)[i]))
-        # print(M[i]@(v)[i,:,argus]/((v)[i,:,argus]*(w)[i,argus]))
-        # print(np.shape((v)[i,:,argus]))
-        W[i]=(w)[i,argus]
-        V[i,:]=(v)[i,:,argus]
-        U[i]=np.abs(np.linalg.det(M[i]))
-    # print(A(n,r0_per_λ0).T(axes=[0,2])[500,:,:])
-    # np.linalg.eig(A(n,r0_per_λ0))
-        # print(V[i,:])
-        # print(np.real(np.inner(np.conj(v[np.argmin(np.abs(w))]),v[np.argmin(np.abs(w))])))
+    argus=np.argmin(np.abs(w),axis=1)
+    W=w[np.arange(N),argus]
+    V=v[np.arange(N),:,argus]
+
 
     # Plots n_eff
-    
-    plt.pcolormesh(2*np.pi*Ro,Ne,-logdetM,cmap='Greys')
-    plt.xlim(xmax=2*np.pi*rmax,xmin=rmin) 
-    plt.plot(2*np.pi*r0_per_λ0,n,label=r'$n_\mathrm{eff}(k_0r_0)$')
-    # plt.plot(2*np.pi*r0_per_λ0,n*g,label=r'has eigenvector')
-    plt.ylim(n2,n1)
-    plt.colorbar(label=r'$-\log_{10}|\det{M}|$')
-    plt.grid(color='black', linestyle='--')
-    plt.ylabel(r'$n_\mathrm{eff}=\lambda_0/\lambda_z$')
-    plt.xlabel(r'$k_0r_0=2\pi r_0/\lambda_0$')
-    plt.legend()
-    plt.show()
-    # bla=np.array([1,2,3,4])
-    plt.grid(color='black', linestyle='--')
-    plt.plot(2*np.pi*r0_per_λ0,(U),label=r'$|\det{M}|$')
-    # plt.plot(2*np.pi*r0_per_λ0,np.log10(np.abs(W)),label=r'$\log_{10}|{\lambda_\mathrm{min}}|$')
-    plt.plot(2*np.pi*r0_per_λ0,(np.abs(w)),label=[f"$|\lambda_1|$",f"$|\lambda_2|$",f"$|\lambda_3|$",f"$|\lambda_4|$",])
-    # plt.axhline(y=np.mean(np.log10(np.abs(W[2*np.pi*r0_per_λ0>1]))),color='black')
-    # plt.plot(2*np.pi*r0_per_λ0,np.log10(U)-np.log10(W),label=r'difference')
-    plt.yscale('log')
-    # plt.ylim(ymin=-10,ymax=5) 
-    plt.legend()
-    plt.show()
+    if neff_fig:
+        plt.pcolormesh(2*np.pi*Ro,Ne,-logdetM,cmap='Greys')
+        plt.xlim(xmax=2*np.pi*rmax,xmin=rmin) 
+        plt.plot(2*np.pi*r0_per_λ0,n,label=r'$n_\mathrm{eff}(k_0r_0)$')
+        # plt.plot(2*np.pi*r0_per_λ0,n*g,label=r'has eigenvector')
+        plt.ylim(n2,n1)
+        plt.colorbar(label=r'$-\log_{10}|\det{M}|$')
+        plt.grid(color='black', linestyle='--')
+        plt.ylabel(r'$n_\mathrm{eff}=\lambda_0/\lambda_z$')
+        plt.xlabel(r'$k_0r_0=2\pi r_0/\lambda_0$')
+        plt.legend()
+        plt.show()
 
-    return (r0_per_λ0,n,V)
+    
+    
+    # plt.grid(color='black', linestyle='--')
+    # plt.plot(2*np.pi*r0_per_λ0,(np.abs(np.linalg.det(M))),label=r'$|\det{M}|$')
+    # plt.plot(2*np.pi*r0_per_λ0,(np.abs(w)),label=[f"$|\lambda_1|$",f"$|\lambda_2|$",f"$|\lambda_3|$",f"$|\lambda_4|$",])
+    # plt.plot(2*np.pi*r0_per_λ0,(np.abs(W)),label=r"$|\lambda_\mathrm{min}|$", linestyle='--')
+    # plt.yscale('log')
+    # # plt.ylim(ymin=-10,ymax=5) 
+    # plt.legend()
+    # plt.show()
+    if r0_over_λ0 is None: 
+        return (r0_per_λ0,n,V)
+    else:
+        idx=int(np.argwhere(r0_per_λ0==find_nearest(r0_per_λ0,r0_over_λ0)))
+        return (r0_per_λ0[idx],n[idx],V[idx])
+
+def get_E(x,y,z,λ0,r0,n_in=1.45,n_out=1):
+    material(n_in,n_out)
+    (r0_per_λ0,n,V)=get_neff(rmin=0,rmax=int(np.ceil(r0/λ0)),N=1000,r0_over_λ0=r0/λ0)
+
+    r=np.sqrt(x**2+y**2)
+    φ=np.arctan2(y,x)
+    k0_r=2*np.pi*r/λ0
+    kz_z=2*np.pi*n*z/λ0
+    κ1_per_k0=np.emath.sqrt(n1**2-n**2) 
+    κ2_per_k0=np.emath.sqrt(n2**2-n**2)
+
+    Ep=np.where(r/r0<1,
+                (-1j/(n1*κ1_per_k0))*(n*V[0]+1j*n1*V[1])*jv(0,k0_r*κ1_per_k0)*np.exp(1j*kz_z),
+                (-1j/(n2*κ2_per_k0))*(n*V[2]+1j*n2*V[3])*hv(0,k0_r*κ2_per_k0)*np.exp(1j*kz_z))
+    Em=np.where(r/r0<1,
+                (-1j/(n1*κ1_per_k0))*(-n*V[0]+1j*n1*V[1])*jv(2,k0_r*κ1_per_k0)*np.exp(1j*(2*φ+kz_z)),
+                (-1j/(n2*κ2_per_k0))*(-n*V[2]+1j*n2*V[3])*hv(2,k0_r*κ2_per_k0)*np.exp(1j*(2*φ+kz_z)))
+    Ez=np.where(r/r0<1,
+                (np.sqrt(2)/n1)*V[0]*jv(1,k0_r*κ1_per_k0)*np.exp(1j*(φ+kz_z)),
+                (np.sqrt(2)/n2)*V[2]*hv(1,k0_r*κ2_per_k0)*np.exp(1j*(φ+kz_z)))
+
+    return ((Ep+Em)/np.sqrt(2),1j*(Ep-Em)/np.sqrt(2),Ez)
+
+def get_H(x,y,z,λ0,r0,n_in=1.45,n_out=1):
+    material(n_in,n_out)
+    (r0_per_λ0,n,V)=get_neff(rmin=0,rmax=int(np.ceil(r0/λ0)),N=1000,r0_over_λ0=r0/λ0)
+
+    r=np.sqrt(x**2+y**2)
+    φ=np.arctan2(y,x)
+    k0_r=2*np.pi*r/λ0
+    kz_z=2*np.pi*n*z/λ0
+    κ1_per_k0=np.emath.sqrt(n1**2-n**2) 
+    κ2_per_k0=np.emath.sqrt(n2**2-n**2)
+
+    Hp=np.where(r/r0<1,
+                (-1j/(κ1_per_k0))*(n*V[1]-1j*n1*V[0])*jv(0,k0_r*κ1_per_k0)*np.exp(1j*kz_z),
+                (-1j/(κ2_per_k0))*(n*V[3]-1j*n1*V[2])*hv(0,k0_r*κ2_per_k0)*np.exp(1j*kz_z))
+    Hm=np.where(r/r0<1,
+                (-1j/(κ1_per_k0))*(-n*V[1]-1j*n1*V[0])*jv(2,k0_r*κ1_per_k0)*np.exp(1j*(2*φ+kz_z)),
+                (-1j/(κ2_per_k0))*(-n*V[3]-1j*n1*V[2])*hv(2,k0_r*κ2_per_k0)*np.exp(1j*(2*φ+kz_z)))
+    Hz=np.where(r/r0<1,
+                np.sqrt(2)*V[1]*jv(1,k0_r*κ1_per_k0)*np.exp(1j*(φ+kz_z)),
+                np.sqrt(2)*V[3]*hv(1,k0_r*κ2_per_k0)*np.exp(1j*(φ+kz_z)))
+
+    return ((Hp+Hm)/np.sqrt(2),1j*(Hp-Hm)/np.sqrt(2),Hz)
 
 # E and H fields
 def E(V,n,r0_per_λ0,x_per_r0,y_per_r0,z_per_r0):
